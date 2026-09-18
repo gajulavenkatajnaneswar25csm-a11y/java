3eb9064c37df5bb1caebb8ebe7509e7fd5006e71
gajulavenkatajnaneswar25csm-a11y <gajulavenkatajnaneswar.25.csm@anits.edu.in>
2026-09-18 21:51:39 +0530

    Add files via upload

---
+++ record/WEEK7/MethodOverride.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+class Customerinfo {
+    String name;
+    int age;
+
+    Customerinfo(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Accountinfo {
+    int accountNo;
+    double balance;
+
+    Accountinfo(int accountNo, double balance) {
+        this.accountNo = accountNo;
+        this.balance = balance;
+    }
+}
+
+class RBI {
+    double getRateOfInterest() {
+        return 4.0;
+    }
+}
+
+class SBI extends RBI {
+    double getRateOfInterest() {
+        return 7.0;
+    }
+}
+
+class ICICI extends RBI {
+    double getRateOfInterest() {
+        return 6.5;
+    }
+}
+
+class PNB extends RBI {
+    double getRateOfInterest() {
+        return 6.0;
+    }
+}
+
+public class MethodOverride {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Bank name to find the rate of Interest : ");
+        String name = sc.nextLine();
+
+        RBI bank;
+
+        if (name.equalsIgnoreCase("SBI")) {
+            bank = new SBI();
+        }
+        else if (name.equalsIgnoreCase("ICICI")) {
+            bank = new ICICI();
+        }
+        else if (name.equalsIgnoreCase("PNB")) {
+            bank = new PNB();
+        }
+        else {
+            bank = new RBI();
+        }
+
+        System.out.println(name.toUpperCase() +
+                " rate of interest is : " +
+                bank.getRateOfInterest() + "%");
+
+        sc.close();
+    }
+}
