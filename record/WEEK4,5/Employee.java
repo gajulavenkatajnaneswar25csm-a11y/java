ef6f99261ab84c984d7978ab6b7a80939ecb7fc9
gajulavenkatajnaneswar25csm-a11y <gajulavenkatajnaneswar.25.csm@anits.edu.in>
2026-09-18 21:46:06 +0530

    Add files via upload

---
+++ record/WEEK4,5/Employee.java
@@ -0,0 +1,36 @@
+public class Employee {
+    String name;
+    int id;
+    String designation;
+    double salary;
+    String promotionStatus;
+
+    Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    Employee(String name, int id, String designation, double salary, String promotionStatus) {
+        this.name = name;
+        this.id = id;
+        this.designation = designation;
+        this.salary = salary;
+        this.promotionStatus = promotionStatus;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(id);
+        System.out.println(designation);
+        System.out.println(salary);
+        System.out.println(promotionStatus);
+    }
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee("venkat", 101);
+        Employee e2 = new Employee("Rahul", 102, "Manager", 50000, "Yes");
+
+        e1.display();
+        e2.display();
+    }
+}
