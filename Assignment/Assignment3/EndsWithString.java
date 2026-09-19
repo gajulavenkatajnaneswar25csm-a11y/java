4cb2690c1573adc944e99149fea7cc7d63b3b9eb
gajulavenkatajnaneswar25csm-a11y <gajulavenkatajnaneswar.25.csm@anits.edu.in>
2026-09-19 22:50:50 +0530

    Add files via upload

---
+++ Assignment/Assignment3/EndsWithString.java
@@ -0,0 +1,8 @@
+class EndsWithString {
+    public static void main(String[] args) {
+        String text1 = "Hello Java";
+        String text2 = "bye Java1";
+        System.out.println(text1.endsWith("Java"));
+        System.out.println(text2.endsWith("Java"));
+    }
+}
