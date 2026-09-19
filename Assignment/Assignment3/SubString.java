73f208665612b38878b8cc2b19875891c18742ee
gajulavenkatajnaneswar25csm-a11y <gajulavenkatajnaneswar.25.csm@anits.edu.in>
2026-09-19 23:40:57 +0530

    Add files via upload

---
+++ Assignment/Assignment3/SubString.java
@@ -0,0 +1,6 @@
+class SubString {
+    public static void main(String[] args) {
+        String text = "malli pudathama enti seppu mey";
+        System.out.println(text.substring(0, 20));
+    }
+}
