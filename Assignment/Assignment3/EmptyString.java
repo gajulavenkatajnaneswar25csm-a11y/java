3c3b40cbb08e0e71daeb64f4fc5b84e218381c55
gajulavenkatajnaneswar25csm-a11y <gajulavenkatajnaneswar.25.csm@anits.edu.in>
2026-09-19 22:45:44 +0530

    Add files via upload

---
+++ Assignment/Assignment3/EmptyString.java
@@ -0,0 +1,8 @@
+class EmptyString {
+    public static void main(String[] args) {
+        String text1 = "";
+        String text2 = "hi";
+        System.out.println(text1.isEmpty());
+        System.out.println(text2.isEmpty());
+    }
+}
