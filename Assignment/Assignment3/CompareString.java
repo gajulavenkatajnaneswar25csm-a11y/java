accf1b67a49f2af2be3f076b5c0be21f792520b4
gajulavenkatajnaneswar25csm-a11y <gajulavenkatajnaneswar.25.csm@anits.edu.in>
2026-09-19 22:39:44 +0530

    Add files via upload

---
+++ Assignment/Assignment3/CompareString.java
@@ -0,0 +1,9 @@
+class CompareString {
+    public static void main(String[] args) {
+        String a = "Java";
+        String b = "Java";
+        String c = "Python";
+        System.out.println(a.equals(b));
+        System.out.println(a.equals(c));
+    }
+}
