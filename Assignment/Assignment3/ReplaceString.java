6fd7eced3ebcadbcf7a0c7f291c5d6501892b07a
gajulavenkatajnaneswar25csm-a11y <gajulavenkatajnaneswar.25.csm@anits.edu.in>
2026-09-19 23:15:21 +0530

    Add files via upload

---
+++ Assignment/Assignment3/ReplaceString.java
@@ -0,0 +1,6 @@
+class ReplaceString {
+    public static void main(String[] args) {
+        String text = "manaku avasarama";
+        System.out.println(text.replace("avasarama", "endhuku"));
+    }
+}
